32361a3b1f2962d4264ff9bc58f075f9f90c8d0d
Angel Arcoraci <angel.arcoraci@gmail.com>
2022-01-14 21:26:28 +0100

    play around

--- src/components/HadleysHope.vue
+++ src/components/HadleysHope.vue
@@ -22,14 +22,14 @@ const createScene = (targetDomElement: Element) => {
   const aspect = width / height;
   scene = new THREE.Scene();
   scene.add(createGround());
-  scene.add(createBox());
+  // scene.add(createBox());
 
   loadModels(scene);
 
   scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
 
   scene.add(new THREE.AmbientLight(0x4000ff));
-  const light = new THREE.PointLight(0xffffff, 6, 40);
+  const light = new THREE.PointLight(0xffffff, 5, 40);
   light.castShadow = true;
   light.position.set(10, 20, 15);
   scene.add(light);
@@ -37,7 +37,7 @@ const createScene = (targetDomElement: Element) => {
   camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, nearPlane, farPlane);
   camera.position.set(20, 20, 20)
   camera.lookAt(scene.position)
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.shadowMap.enabled = true;
   renderer.setSize(window.innerWidth, window.innerHeight);
   targetDomElement.appendChild(renderer.domElement);
@@ -73,6 +73,10 @@ const createBox = (): THREE.Mesh => {
 
 const loadModels = (scene: THREE.Scene) => {
   loader.load('./models/ambient.gltf', function (gltf) {
+    gltf.scene.traverse(function (node) {
+      if (node.isObject3D) { node.castShadow = true; node.receiveShadow = true; }
+    });
+    gltf.scene.rotateY(Math.PI * .15);
     scene.add(gltf.scene);
   }, undefined, function (error) {
     console.error(error);
