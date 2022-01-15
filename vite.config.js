8658eaf5491ecc2a307a50585946fa48ba181f3f
Angel Arcoraci <angel.arcoraci@gmail.com>
2022-01-15 21:59:21 +0100

    global scss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // example : additionalData: `@import "./src/design/styles/variables";`
+        // dont need include file extend .scss
+        additionalData: `
+          @import "./src/assets/scss/partials/variables/_colors.scss";
+          @import "./src/assets/scss/partials/utils/_breakpoints.scss";
+          @import "./src/assets/scss/partials/utils/_typography.scss";
+          `,
+      },
+    },
+  },
+});
