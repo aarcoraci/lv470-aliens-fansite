4805ca140cf58541a7f92f410c83313e9c58a506
Angel Arcoraci <angel.arcoraci@gmail.com>
2022-01-29 17:13:57 +0100

    prepare to work on a subdir

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
-import HadleysHope from "../views/HadleysHope.vue";
-import About from "../views/About.vue";
+import HadleysHope from '../views/HadleysHope.vue';
+import About from '../views/About.vue';
 
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from 'vue-router';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: HadleysHope,
+    path: '/',
+    name: 'Home',
+    component: HadleysHope
   },
   {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
+    path: '/about',
+    name: 'About',
+    component: About
+  }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 });
 
 export default router;
--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,31 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '/src'),
-      '~@': path.resolve(__dirname, '/src')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // example : additionalData: `@import "./src/design/styles/variables";`
-        // dont need include file extend .scss
-        additionalData: `
+export default defineConfig(({ mode }) => {
+  const basePath = mode === 'production' ? '/lv470-aliens-fansite/' : '/';
+  return {
+    plugins: [vue()],
+    base: mode === 'production' ? '/lv470-aliens-fansite/' : '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '/src'),
+        '~@': path.resolve(__dirname, '/src')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // example : additionalData: `@import "./src/design/styles/variables";`
+          // dont need include file extend .scss
+          additionalData: `
           @import "./src/assets/scss/partials/variables/_layout.scss";
           @import "./src/assets/scss/partials/variables/_colors.scss";
           @import "./src/assets/scss/partials/utils/_breakpoints.scss";
           @import "./src/assets/scss/partials/utils/_typography.scss";
           @import "./src/assets/scss/partials/_fonts.scss";
           `
+        }
       }
     }
-  }
+  };
 });
