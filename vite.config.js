ebf4a240fc8b1c45d724223f9a46a8d8e17288de
Angel Arcoraci <angel.arcoraci@gmail.com>
2022-01-29 21:45:22 +0100

    small code updates and clean up

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const basePath = mode === 'production' ? '/lv470-aliens-fansite/' : '/';
   return {
     plugins: [vue()],
     base: mode === 'production' ? '/lv470-aliens-fansite/' : '/',
